82b4eaf8944bffaaac66c344ded13cf91ccce6b5
triddd <zjatko@mail.ru>
2016-07-04 12:47:04 +0300

    finish mobile markup

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,47 @@
 $(function(){
 "use strict";
 
-	$("button").click( function(e){
-		e.preventDefault();
+	$("header button").click( function(e){
 		var id = e.target,
 			ul;
+		console.log($( "#headerItemNav" ));
+		if( id.id === "headerNavBut" || id.parentNode.id === "headerNavBut"){
+			$( "#headerItemNav" ).focus();
+			console.log($( "#headerItemNav" ));
+		}
+		else if( id.id === "headerSocialBut" || id.parentNode.id === "headerSocialBut" ){
+			ul = $( "#socialNav" );
+			ul.trigger("focus");
+		}
+	});
 
-		//console.log( id.id );
-		if( id.id === "headerNavBut" ){
-			ul = $( ".headerNav" );
-			if( ul.hasClass( "dropDownNav" ) )
-				ul.removeClass( "dropDownNav" );
-			else
-				ul.addClass( "dropDownNav" );
-		}
-		else if( id.id === "headerSocialBut" ){
-			ul = $( ".socialNav" );
-			if( ul.hasClass( "dropDownNav" ) )
-				ul.removeClass( "dropDownNav" );
-			else
-				ul.addClass( "dropDownNav" );
-		}
+	$( "#headerItemNav" ).focus(function(e){
+		
+		console.log($( "#headerItemNav" ));
+		if( $("#headerNav").hasClass( "dropDownNav" ) )
+			$("#headerNav").removeClass( "dropDownNav" );
+		else
+			$("#headerNav").addClass( "dropDownNav" );
+	});
+
+
+	$( "#headerItemNav" ).blur( function(e){
+		$("#headerNav").addClass( "dropDownNav" );
+	});
+
+	$( "#socialNav" ).blur( function(e){
+		$( this ).addClass( "dropDownNav" );
+	});
+
+	
+
+
+
+	$('.cover').liCover({
+	    parent: $("#galleryId li "),
+	    position:'absolute',
+	    veticalAlign:'middle',
+	    align:'center'
 	});
 
 });
